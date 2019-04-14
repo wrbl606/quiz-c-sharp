bc49940a6531c6f53e147d5b4b922577bf37bc01
Marcin Wróblewski <marcinwroblewski@outlook.com>
2019-04-14 20:56:37 +0200

    initial

---
+++ Answer.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+public class Answer
+{
+    public string Content { get; set; }
+    public bool IsCorrect { get; set; }
+}
---
+++ Question.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Question
+{
+    public string Content { get; set; }
+    public decimal CorrectAnswerPoints { get; set; } = 1;
+    public decimal IncorrectAnswerPoints { get; set; } = -1;
+    public List<Answer> Answers { get; } = new List<Answer>();
+}
